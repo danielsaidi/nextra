39c63323ae130de4539d57f92ef96c9828273d40
Daniel Saidi <daniel.saidi@gmail.com>
2011-11-27 23:03:02 +0100

    Adjusted documentation

---
+++ NExtra/Geo/GeoConstants.cs
@@ -0,0 +1,22 @@
+﻿namespace NExtra.Geo
+{
+    /// <summary>
+    /// This static class contains geo-related constants.
+    /// </summary>
+    /// <remarks>
+    /// Author:     Daniel Saidi [daniel.saidi@gmail.com]
+    /// Link:       http://www.saidi.se/nextra
+    /// </remarks>
+    public static class GeoConstants
+    {
+        /// <summary>
+        /// The rough earth radius in kilometers.
+        /// </summary>
+        public static double EarthRadiusInKilometers { get { return 6367.0; } }
+
+        /// <summary>
+        /// The rough earth radius in miles.
+        /// </summary>
+        public static double EarthRadiusInMiles { get { return 3956.0; } }
+    }
+}
