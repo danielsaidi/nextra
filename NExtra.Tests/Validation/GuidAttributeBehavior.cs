2b51b2e34acdb0d202d73512cd0362200bce4464
Daniel Saidi <daniel.saidi@gmail.com>
2012-01-05 10:29:07 +0100

    Made GUID validator implement IValidator

--- NExtra.Tests/Validation/GuidAttributeBehavior.cs
+++ NExtra.Tests/Validation/GuidAttributeBehavior.cs
@@ -7,29 +7,39 @@ namespace NExtra.Tests.Validation
 	[TestFixture]
 	public class GuidAttributeBehavior
     {
+        private IValidator validator;
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            validator = new GuidAttribute();
+        }
+
+
         [Test]
         public void IsValid_ShouldReturnTrueForNullValue()
         {
-            Assert.That(new GuidAttribute().IsValid(null), Is.True);
+            Assert.That(validator.IsValid(null), Is.True);
         }
 
         [Test]
         public void IsValid_ShouldReturnTrueForEmptyString()
         {
-            Assert.That(new GuidAttribute().IsValid(""), Is.True);
+            Assert.That(validator.IsValid(""), Is.True);
         }
 
 		[Test]
 		public void IsValid_ShouldReturnFalseForInvalidString()
 		{
-			Assert.That(new GuidAttribute().IsValid("weghighuihgaweuihgweui"), Is.False);
+			Assert.That(validator.IsValid("weghighuihgaweuihgweui"), Is.False);
 		}
 
 		[Test]
 		public void IsValid_ShouldReturnTrueForValidStrings()
 		{
-			Assert.That(new GuidAttribute().IsValid(Guid.Empty), Is.True);
-			Assert.That(new GuidAttribute().IsValid(Guid.NewGuid()), Is.True);
+			Assert.That(validator.IsValid(Guid.Empty), Is.True);
+			Assert.That(validator.IsValid(Guid.NewGuid()), Is.True);
 		}
 	}
 }
--- NExtra/Validation/GuidAttribute.cs
+++ NExtra/Validation/GuidAttribute.cs
@@ -10,7 +10,7 @@ namespace NExtra.Validation
 	/// Author:     Daniel Saidi [daniel.saidi@gmail.com]
 	/// Link:       http://www.saidi.se/nextra
 	/// </remarks>
-	public class GuidAttribute : RegularExpressionAttribute
+    public class GuidAttribute : RegularExpressionAttribute, IValidator
     {
 		public GuidAttribute()
             : base(Expression) { }
