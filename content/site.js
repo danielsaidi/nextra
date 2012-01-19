77a5180c7848b42439e44ab8693e0d548291e1a2
Daniel Saidi <daniel.saidi@gmail.com>
2012-01-19 01:15:32 +0100

    Finished the new design

--- content/site.js
+++ content/site.js
@@ -1,3 +1,5 @@
+if (location.href == "http://danielsaidi.github.com/NExtra");
+
 $(document).ready(function() {
   var repo = new gh.repo("danielsaidi", "NExtra");
   var tags = repo.getLatestRelease(function(result){ $("#version").html(result); });
