370807e449d4957a49e84a5b80c0e23ce7afce34
Niklas Melinder <niklas@melinder.se>
2012-02-22 12:53:30 +0100

    Added ITypeLocator and implementation

---
+++ NExtra/Reflection/ITypeLocator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace NExtra.Reflection
+{
+    /// <summary>
+    /// Interface that can be implemented by classes
+    /// that are used to find all types that implement
+    /// a specific interface. 
+    /// </summary>
+    /// <typeparam name="TType">The interface type to find.</typeparam>
+    public interface ITypeLocator<out TType>
+    {
+        IEnumerable<TType> FindAll();
+    }
+}
