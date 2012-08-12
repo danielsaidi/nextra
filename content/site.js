4106842fd9a5c417110d5d678e4d4f4fcd3dcaf8
Daniel Saidi <daniel.saidi@gmail.com>
2012-08-12 12:08:21 +0200

    Removed redirect for gh

--- content/site.js
+++ content/site.js
@@ -1,9 +1,6 @@
-if (location.href == "http://danielsaidi.github.com/NExtra/")
-	location.href = "http://www.dotnextra.com";
-
 $(document).ready(function() {
   var repo = new gh.repo("danielsaidi", "NExtra");
-  var tags = repo.getLatestRelease(function(result){ $("#version").html(result); });
+  var tags = repo.getLatestRelease(function(result){ $("#version").html("(v. " + result + ")"); });
   
   $("a[rel='external']").attr("target", "_blank");
 });
