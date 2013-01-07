5d435edd5093b2f9b7ba67a10a74b112012e894e
Daniel Saidi <daniel.saidi@gmail.com>
2013-01-07 23:35:18 +0100

    Completely removed the EPiServer library

--- NExtra.EpiServer/NExtra.EpiServer/Profile/Abstractions/ICanCreateEPiServerProfile.cs
+++
@@ -1,9 +0,0 @@
-using EPiServer.Personalization;
-
-namespace NExtra.EpiServer.Profile.Abstractions
-{
-    public interface ICanCreateEPiServerProfile
-    {
-        EPiServerProfile CreateEPiServerProfile(string userName, string company, string email, string firstName, string lastName, string title);
-    }
-}
