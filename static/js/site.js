f56417811c0b74ea726f86e13e1c85b63cce4a7b
Daniel Saidi <daniel.saidi@gmail.com>
2013-01-23 23:44:39 +0100

    Minor adjs

--- static/js/site.js
+++ static/js/site.js
@@ -8,13 +8,11 @@ $(document).ready(function() {
 		var api_base = "https://api.github.com/repos/danielsaidi/nextra";
 		var api_tags = api_base + "/tags?callback=?";
 
-		//
-		//updateVersionInfo("2.6.5");
-		//
-		
-		$.get(api_tags, function(data) {
+		updateVersionInfo("3.0.0");
+
+		/*$.get(api_tags, function(data) {
 			updateVersionInfo(data[0]["name"]);
-		});
+		});*/
 	}
 
 	function updateVersionInfo(version) {
