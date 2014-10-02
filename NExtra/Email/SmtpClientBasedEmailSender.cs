e32f6e4ba80e480a394c193c9846c1710abd0865
Daniel Saidi <daniel.saidi@gmail.com>
2014-10-02 09:54:09 +0200

    [style] Rename EmailSender to more descriptive name

---
+++ NExtra/Email/SmtpClientBasedEmailSender.cs
@@ -0,0 +1,30 @@
+using System.Net;
+using System.Net.Mail;
+
+namespace NExtra.Email
+{
+    /// <summary>
+    /// This class can be used to send an e-mail message,
+    /// using the default SmtpClient.
+    /// 
+    /// To enable it, configure the SMTP settings in the
+    /// configuration file for the project, e.g. in your
+    /// web.config file.
+    /// </summary>
+    /// <remarks>
+    /// Author:     Daniel Saidi [daniel.saidi@gmail.com]
+    /// Link:       http://danielsaidi.github.com/nextra
+    /// </remarks>
+    public class SmtpClientBasedEmailSender : IEmailSender
+    {
+        public void SendEmail(string fromName, string fromEmail, string toEmail, string subject, string body)
+        {
+            SendEmail(new MailMessage(fromEmail, toEmail, subject, body));
+        }
+
+        public void SendEmail(MailMessage mailMessage)
+        {
+            new SmtpClient { Credentials = CredentialCache.DefaultNetworkCredentials }.Send(mailMessage);
+        }
+    }
+}
